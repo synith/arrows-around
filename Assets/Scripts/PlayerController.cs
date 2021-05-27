a8bd92c49d23d4ae0a1a34e2ff21e7bf62d09f17
Synith <synith@live.ca>
2021-05-26 21:53:33 -0600

    moved volume slider in scene, cleaned up playercontroller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,137 +5,91 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    public AudioSource playerAudio;
+    public bool
+        isDead = false,
+        isHit = false,
+        shieldIsHit = false,
+        shieldUp = false;
+    public int
+        shieldHits = 0,
+        bodyHits = 0;
+    
+    private Rigidbody playerRb;
+    private Vector3 moveDirection;
+    private GameObject
+        shieldObject,
+        bodyObject;
+    private Animator playerAnimator;
+    private GameManager gameManager;
+    private bool
+        deathCry = false,
+        shieldBroken;
+    private readonly int
+        playerMaxhp = 5,
+        shieldMaxhp = 3;
+    private int
+        playerHp = 5,
+        shieldHp = 3;
+
     [SerializeField]
     private float
         speed,
+        rotateSpeed,
         xRange,
-        zRange,
-        rotateSpeed;
-
-    private Vector3 moveDirection;
-    private Rigidbody playerRb;
-
-    public int
-        shieldHits,
-        bodyHits;
-
-    public bool shieldUp;
-    private GameObject shieldObject;
-    private GameObject bodyObject;
-
-    private Animator playerAnimator;
-    private GameManager gameManager;
-
-    public AudioSource playerAudio;
-    [SerializeField] private AudioClip deathSound;
-    [SerializeField] private AudioClip hitSound;
-    [SerializeField] private AudioClip pickupSound;
-
-    public bool isDead;
-    public bool isHit;
-    public bool shieldIsHit;
-    private bool deathCry;
-
-    [SerializeField] private ParticleSystem bloodParticle;
-    [SerializeField] private ParticleSystem deathParticle;
-
-    [SerializeField] private HealthBar healthBar;
-    [SerializeField] private HealthBar shieldbar;
-
+        zRange;
+    [SerializeField]
+    private HealthBar
+        healthBar,
+        shieldbar;
+    [SerializeField]
+    private AudioClip
+        deathSound,
+        hitSound,
+        pickupSound;
+    [SerializeField]
+    private ParticleSystem
+        bloodParticle,
+        deathParticle;
     
-    private bool shieldBroken;
-    private readonly int playerMaxhp = 5;
-    private readonly int shieldMaxhp = 3;
-    private int playerHp = 5;
-    private int shieldHp = 3;
-
-
     private void Awake()
     {
         playerAnimator = GetComponent<Animator>();
         playerAudio = GetComponent<AudioSource>();
         shieldObject = GameObject.Find("Shield");
         bodyObject = GameObject.Find("Body");
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
-
-    // Start is called before the first frame update
     public void PlayerControllerStart()
     {
-        playerRb = GetComponent<Rigidbody>();
-        isDead = false;
-        deathCry = false;
-        shieldUp = false;
-        isHit = false;
-        shieldIsHit = false;
-        shieldHits = 0;
-        bodyHits = 0;
+        playerRb = GetComponent<Rigidbody>();        
         shieldObject.SetActive(false);
         healthBar.SetMaxHealth(playerMaxhp);
         shieldbar.SetMaxHealth(shieldMaxhp);
+        bodyHits = 0;
+        shieldHits = 0;        
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Input
         PlayerInput();
-
-        // Boundary
         ConstrainPlayer();
-
-        // Shield Block
-        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
-            shieldUp = true;
-        else
-            shieldUp = false;        
+        ShieldBlock();
     }
     private void FixedUpdate()
     {
         CheckPlayerHP();
-        CheckShieldHP();       
-
-        // if game not over
+        CheckShieldHP();
         if (!gameManager.gameOver && gameManager.gameStarted)
         {
-            // if shield not broken, allow shield to spawn.
-            if (!shieldBroken)
-                shieldObject.SetActive(shieldUp);
-            else
-                shieldObject.SetActive(false);
-
-            // Movement
-            playerRb.AddForce(moveDirection * speed);
-
-            // Rotation
+            CheckShieldBroken();
+            MovePlayer();
             RotateTowardsMovementDirection();
         }
-
         CheckIfHit();
-    }
-    // Rotates player to direction they are moving in
-    private void RotateTowardsMovementDirection()
-    {
-        // if moving in a direction
-        if (moveDirection.magnitude != 0)
-        {
-            playerAnimator.SetTrigger("walking_trig");            
-
-            // Quaternion using vector to represent where we want to rotate to
-            Quaternion rotatePlayer = Quaternion.LookRotation(moveDirection);
-
-            // With physics:
-            // rotate rigidbody from current rotation to target rotation at a set speed degrees/second
-            playerRb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotatePlayer, rotateSpeed));
-
-            // Without physics:
-            //transform.rotation = Quaternion.RotateTowards(transform.rotation, rotatePlayer, rotateSpeed);            
-        }
-    }
-    // Sets horizontal and vertical input as a vector
+    }    
     private void PlayerInput()
-    {
-        // a vector based on our x and y input
+    {        
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveDirection.Normalize();
     }
@@ -150,6 +104,14 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
         if (transform.position.z > zRange)
             transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+    }
+    private void ShieldBlock()
+    {
+        // Shield Block on space bar or left mouse button
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Fire1"))
+            shieldUp = true;
+        else
+            shieldUp = false;
     }
     private void CheckPlayerHP()
     {
@@ -174,6 +136,28 @@ public class PlayerController : MonoBehaviour
 
         if (shieldHp > 0)
             shieldBroken = false;
+    }
+    private void CheckShieldBroken()
+    {
+        // if shield is broken space bar does nothing
+        if (!shieldBroken)
+            shieldObject.SetActive(shieldUp);
+        else
+            shieldObject.SetActive(false);
+    }
+    private void MovePlayer()
+    {
+        playerRb.AddForce(moveDirection * speed);
+    }
+    // Rotates player to direction they are moving in
+    private void RotateTowardsMovementDirection()
+    {
+        if (moveDirection.magnitude != 0)
+        {
+            playerAnimator.SetTrigger("walking_trig");
+            Quaternion rotatePlayer = Quaternion.LookRotation(moveDirection);
+            playerRb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotatePlayer, rotateSpeed));
+        }
     }
     private void CheckIfHit()
     {
@@ -221,7 +205,7 @@ public class PlayerController : MonoBehaviour
                 Destroy(other.gameObject);
                 shieldIsHit = true;
                 playerAudio.PlayOneShot(pickupSound, 0.3f * gameManager.volume);
-            }                
+            }
         }
     }
 }
